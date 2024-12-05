3d9ba0faf1982d6cf7bea1c5c1eccecbff46822c
Luxibris19 <101341654+Luxibris19@users.noreply.github.com>
2024-12-05 18:10:38 -0300

    Cambios minimos

--- fitzencapstone/settings.py
+++ fitzencapstone/settings.py
@@ -85,15 +85,15 @@ WSGI_APPLICATION = 'fitzencapstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
@@ -107,6 +107,7 @@ DATABASES = {
         },
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
